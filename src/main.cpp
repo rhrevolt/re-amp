0696b315f7dec33d524e0da68053b7ef268add5a
Nicholas Kamper <nick@nick125.com>
2011-03-13 21:50:26 -0400

    Cleaned up some spaces/tabs from when I moved stuff to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,27 @@ extern "C" {
 #endif
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-    INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+	INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
 #else
-    int main(int argc, char *argv[])
+	int main(int argc, char *argv[])
 #endif
-    {
-        // Create application object
-        Application app;
+	{
+		// Create application object
+		Application app;
 
-        try {
-            app.go();
-        } catch( Ogre::Exception& e ) {
+		try {
+			app.go();
+		} catch( Ogre::Exception& e ) {
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-            MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+			MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
 #else
-            std::cerr << "An exception has occured: " <<
-                e.getFullDescription().c_str() << std::endl;
+			std::cerr << "An exception has occured: " <<
+				e.getFullDescription().c_str() << std::endl;
 #endif
-        }
+		}
 
-        return 0;
-    }
+		return 0;
+	}
 
 #ifdef __cplusplus
 }
