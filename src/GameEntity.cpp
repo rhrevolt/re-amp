b08957aa88fe50f6eb137ea8edfed0f56ae9ce54
Nicholas Kamper <nick@nick125.com>
2011-03-20 15:12:45 -0400

    * Updated license header

--- src/GameEntity.cpp
+++ src/GameEntity.cpp
@@ -1,7 +1,20 @@
-/**
- * Re-Amp Project
- * Game Entity Model
- * (C) 2011 Re-Amp Contributors 
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * ReAmp
+ * Copyright (C)  2011 ReAmp Contributors
+ * 
+ * ReAmp is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ReAmp is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
  
 #include "GameEntity.h"
