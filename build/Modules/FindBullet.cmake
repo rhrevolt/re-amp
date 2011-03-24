794ac0c5b84fc42af1b98d6ed10c4dcd8cbce994
Nicholas Kamper <nick@nick125.com>
2011-03-24 16:59:37 -0400

    * Fixed some of the horribly broken cmake files

--- build/Modules/FindBullet.cmake
+++ build/Modules/FindBullet.cmake
@@ -14,52 +14,48 @@
 #
 
 #=============================================================================
+# Copyright 2011 ReAmp Contributors
 # Copyright 2009 Kitware, Inc.
 # Copyright 2009 Philip Lowman <philip@yhbt.com>
 #
 # Distributed under the OSI-approved BSD License (the "License");
-# see accompanying file Copyright.txt for details.
+# see accompanying file CMake-Copyright.txt for details.
 #
 # This software is distributed WITHOUT ANY WARRANTY; without even the
 # implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the License for more information.
-#=============================================================================
-# (To distribute this file outside of CMake, substitute the full
-#  License text for the above reference.)
+#
+
+find_package(PkgConfig)
+if (PKG_CONFIG_FOUND)
+	pkg_check_modules(PC_BULLET bullet)
+	set(BULLET_DEFINITIONS ${PC_BULLET_CFLAGS_OTHER})
+endif(PKG_CONFIG_FOUND)
+
+find_path(BULLET_INCLUDE_DIR 
+	NAMES btBulletCollisionCommon.h
+	HINTS ${PC_BULLET_INCLUDEDIR} ${PC_BULLET_INCLUDE_DIRS}
+	PATHS ${BULLET_ROOT}
+)
 
 macro(_FIND_BULLET_LIBRARY _var)
-  find_library(${_var}
-     NAMES 
-        ${ARGN}
-     PATHS
-	/usr
-	/usr/local
-        ${BULLET_ROOT}
-        ${BULLET_ROOT}/out/release8/libs
-        ${BULLET_ROOT}/out/debug8/libs
-     PATH_SUFFIXES lib
-  )
-  mark_as_advanced(${_var})
+	find_library(${_var}
+		NAMES ${ARGN}
+		HINTS ${PC_BULLET_LIBDIR} ${PC_BULLET_LIBRARY_DIRS}
+		PATHS ${BULLET_ROOT}
+		PATH_SUFFIXES lib
+	)
+	mark_as_advanced(${_var})
 endmacro()
 
 macro(_BULLET_APPEND_LIBRARIES _list _release)
-   set(_debug ${_release}_DEBUG)
-   if(${_debug})
-      set(${_list} ${${_list}} optimized ${${_release}} debug ${${_debug}})
-   else()
-      set(${_list} ${${_list}} ${${_release}})
-   endif()
+	set(_debug ${_release}_DEBUG)
+	if(${_debug})
+		set(${_list} ${${_list}} optimized ${${_release}} debug ${${_debug}})
+	else()
+		set(${_list} ${${_list}} ${${_release}})
+	endif()
 endmacro()
-
-find_path(BULLET_INCLUDE_DIR NAMES btBulletCollisionCommon.h
-  PATHS
-    /usr/local/include
-    /usr/local/include/bullet
-    /usr/include/bullet
-    /usr/include
-    ${BULLET_ROOT}/include
-    ${BULLET_ROOT}/src
-)
 
 # Find the libraries
 
@@ -75,7 +71,6 @@ _FIND_BULLET_LIBRARY(BULLET_SOFTBODY_LIBRARY_DEBUG  BulletSoftBody_d)
 
 # handle the QUIETLY and REQUIRED arguments and set BULLET_FOUND to TRUE if 
 # all listed variables are TRUE
-include("${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake")
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Bullet DEFAULT_MSG
     BULLET_DYNAMICS_LIBRARY BULLET_COLLISION_LIBRARY BULLET_MATH_LIBRARY
     BULLET_SOFTBODY_LIBRARY BULLET_INCLUDE_DIR)
--- build/Modules/FindFreeAlut.cmake
+++ build/Modules/FindFreeAlut.cmake
@@ -1,87 +1,31 @@
-# this module was taken from http://noget.net/cgd/oe/extensions/Sound/conf/cmake/FindFreeAlut.cmake
-#
-# - Locate FreeAlut
 # This module defines
 #  FREEALUT_LIBRARY
 #  FREEALUT_FOUND, if false, do not try to link to FreeAlut 
 #  FREEALUT_LIB_DIR
 #  FREEALUT_INCLUDE_DIR, where to find the headers
 #   
-# $FREEALUTDIR is an environment variable that would
 
-#Todo: Setting up and testing for other platforms than windows
-FIND_PATH(FREEALUT_INCLUDE_DIR alut.h
-  ${FREEALUTDIR}/include/AL
-  
-  ~/Library/Frameworks/freealut.framework/Headers
-  /Library/Frameworks/freealut.framework/Headers
-  /System/Library/Frameworks/freealut.framework/Headers # Tiger
-  /usr/local/include/AL
-  /usr/local/include/freealut
-  /usr/local/include
-  /usr/include/AL
-  /usr/include/freealut
-  /usr/include
-  /sw/include/AL # Fink
-  /sw/include/freealut
-  /sw/include
-  /opt/local/include/AL # DarwinPorts
-  /opt/local/include/freealut
-  /opt/local/include
-  /opt/csw/include/AL # Blastwave
-  /opt/csw/include/freealut
-  /opt/csw/include
-  /opt/include/AL
-  /opt/include/freealut
-  /opt/include
+find_package(PkgConfig)
+if (PKG_CONFIG_FOUND)
+	pkg_check_modules(PC_FREEALUT freealut)
+	set(FREEALUT_DEFINITIONS ${PC_FREEALUT_CFLAGS_OTHER})
+endif(PKG_CONFIG_FOUND)
 
-  ${PROJECT_SOURCE_DIR}/libraries/freealut/include/AL
- )
+find_path(FREEALUT_INCLUDE_DIR alut.h
+	HINTS ${PC_FREEALUT_INCLUDEDIR} ${PC_FREEALUT_INCLUDE_DIRS} 
+	PATHS ${FREEALUT_ROOT}/include/AL 
+)
 
-FIND_PATH(FREEALUT_LIB_DIR alut.lib
-  ${PROJECT_SOURCE_DIR}/libraries/freealut/lib/
-  ${FREEALUTDIR}/lib
+find_path(FREEALUT_LIB_DIR alut.lib
+	HINTS ${PC_FREEALUT_LIBDIR} ${PC_FREEALUT_INCLUDE_DIRS} 
+	PATHS ${FREEALUT_ROOT}/lib
 )  
-  
-# I'm not sure if I should do a special casing for Apple. It is 
-# unlikely that other Unix systems will find the framework path.
-# But if they do ([Next|Open|GNU]Step?), 
-# do they want the -framework option also?
-IF(${FREEALUT_INCLUDE_DIR} MATCHES ".framework")
-  STRING(REGEX REPLACE "(.*)/.*\\.framework/.*" "\\1" FREEALUT_FRAMEWORK_PATH_TMP ${FREEALUT_INCLUDE_DIR})
-  IF("${FREEALUT_FRAMEWORK_PATH_TMP}" STREQUAL "/Library/Frameworks"
-      OR "${FREEALUT_FRAMEWORK_PATH_TMP}" STREQUAL "/System/Library/Frameworks"
-      )
-    # String is in default search path, don't need to use -F
-    SET (FREEALUT_LIBRARY "-framework FreeAlut" CACHE STRING "FreeAlut framework for OSX")
-  ELSE("${FREEALUT_FRAMEWORK_PATH_TMP}" STREQUAL "/Library/Frameworks"
-      OR "${FREEALUT_FRAMEWORK_PATH_TMP}" STREQUAL "/System/Library/Frameworks"
-      )
-    # String is not /Library/Frameworks, need to use -F
-    SET(FREEALUT_LIBRARY "-F${FREEALUT_FRAMEWORK_PATH_TMP} -framework FreeAlut" CACHE STRING "FreeAlut framework for OSX")
-  ENDIF("${FREEALUT_FRAMEWORK_PATH_TMP}" STREQUAL "/Library/Frameworks"
-    OR "${FREEALUT_FRAMEWORK_PATH_TMP}" STREQUAL "/System/Library/Frameworks"
-    )
-  # Clear the temp variable so nobody can see it
-  SET(FREEALUT_FRAMEWORK_PATH_TMP "" CACHE INTERNAL "")
 
-ELSE(${FREEALUT_INCLUDE_DIR} MATCHES ".framework")
-  FIND_LIBRARY(FREEALUT_LIBRARY 
-    NAMES alut
-    PATHS
-    ${FREEALUTDIR}/lib
-    ${FREEALUTDIR}/libs
-    /usr/local/lib
-    /usr/lib
-    /sw/lib
-    /opt/local/lib
-    /opt/csw/lib
-    /opt/lib
-    ${PROJECT_SOURCE_DIR}/libraries/freealut/lib/
-    )    
-ENDIF(${FREEALUT_INCLUDE_DIR} MATCHES ".framework")
-SET(FREEALUT_FOUND "NO")
-IF(FREEALUT_LIBRARY)
-  SET(FREEALUT_FOUND "YES")
-ENDIF(FREEALUT_LIBRARY)
+find_library(FREEALUT_LIBRARY
+	NAMES alut
+	HINTS ${PC_FREEALUT_LIBDIR} ${PC_FREEALUT_LIBRARY_DIRS}
+	PATHS ${FREEALUT_ROOT}/lib
+)
 
+find_package_handle_standard_args(FreeAlut DEFAULT_MSG FREEALUT_LIBRARY FREEALUT_INCLUDE_DIR)
+mark_as_advanced(FREEALUT_INCLUDE_DIR FREEALUT_LIBRARY)
