7b0a26fa5b73428f821bd74bf334546c45f32b20
Jonathon Strauser <strausjc@rose-hulman.edu>
2011-03-24 23:09:31 -0400

    Added setParentEntity to GameComponent

--- include/core/GameComponent.h
+++ include/core/GameComponent.h
@@ -13,14 +13,20 @@ class GameComponent
 		~GameComponent(void);
 		bool recieveMessage(int message);
 		int getPriority(void);
+		bool setParentEntity(GameEntity* parent);
 
 		virtual bool tick(void) = 0;
 
 		ComponentType type;
 		
+	protected:
+		GameEntity* parentEntity;
+		
 	private:
 		int componentID;
 		int priority; //TODO: Remove?
+		
+		
 };
 
 #endif // __GameComponent_h__
