faa0f452a133abeedeabaeadf45db9d60fc16cbf
Nicholas Kamper <nick@nick125.com>
2011-03-27 23:59:08 -0400

    * Added -Wall when the target is Debug

--- cmake/DefineCMakeDefaults.cmake
+++ cmake/DefineCMakeDefaults.cmake
@@ -21,3 +21,8 @@ if (NOT CMAKE_BUILD_TYPE)
 		"Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel."
 	)
 endif (NOT CMAKE_BUILD_TYPE)
+
+if (CMAKE_BUILD_TYPE MATCHES Debug)
+	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
+endif(CMAKE_BUILD_TYPE MATCHES Debug)
